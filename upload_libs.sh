0648f16c49c0b006573b75df8742b3a1bc8c3a51
David Brooks <dave@bcs.co.nz>
2015-12-17 10:26:37 +1300

    Drop `_d` postfix on Windows' debug binaries.

--- upload_libs.sh
+++ upload_libs.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 #
 function upload {
-  openssl dgst -sha1 $1 >> sha1.txt
+  openssl dgst -sha1 $1 >> ../sha1.txt
   tar czf $1.tar.gz -L $1
   scp $1.tar.gz bcs:/www/biosignalml/binaries/src/plugins/api/BioSignalMLAPI/$2
   }
@@ -14,44 +14,49 @@ cp -pr /Users/dave/biosignalml/typedobject/include/typedobject/*    \
        /Users/dave/build/OpenCOR/src/plugins/api/BioSignalMLAPI/include/typedobject
 #
 rm -rf /tmp/upload
-mkdir /tmp/upload
+mkdir -p /tmp/upload/release
+mkdir -p /tmp/upload/debug
 #
-cp /Users/dave/biosignalml/libbsml/win64/biosignalml.lib          /tmp/upload
-cp /Users/dave/biosignalml/libbsml/win64/biosignalml.dll          /tmp/upload
-cp /Users/dave/biosignalml/typedobject/win64/typedobject.lib 	  /tmp/upload
-cp /Users/dave/biosignalml/typedobject/win64/typedobject.dll 	  /tmp/upload
+cp /Users/dave/biosignalml/libbsml/win64/biosignalml.lib          /tmp/upload/release
+cp /Users/dave/biosignalml/libbsml/win64/biosignalml.dll          /tmp/upload/release
+cp /Users/dave/biosignalml/typedobject/win64/typedobject.lib 	  /tmp/upload/release
+cp /Users/dave/biosignalml/typedobject/win64/typedobject.dll 	  /tmp/upload/release
 #
-cp /Users/dave/biosignalml/libbsml/win64d/biosignalml_d.lib       /tmp/upload
-cp /Users/dave/biosignalml/libbsml/win64d/biosignalml_d.dll       /tmp/upload
-cp /Users/dave/biosignalml/typedobject/win64d/typedobject_d.lib   /tmp/upload
-cp /Users/dave/biosignalml/typedobject/win64d/typedobject_d.dll   /tmp/upload
+cp /Users/dave/biosignalml/libbsml/win64d/biosignalml_d.lib       /tmp/upload/debug/biosignalml.lib
+cp /Users/dave/biosignalml/libbsml/win64d/biosignalml_d.dll       /tmp/upload/debug/biosignalml.dll
+cp /Users/dave/biosignalml/typedobject/win64d/typedobject_d.lib   /tmp/upload/debug/typedobject.lib
+cp /Users/dave/biosignalml/typedobject/win64d/typedobject_d.dll   /tmp/upload/debug/typedobject.dll
 #
-cp /Users/dave/biosignalml/libbsml/osx/libbiosignalml.0.dylib     /tmp/upload
-cp /Users/dave/biosignalml/typedobject/osx/libtypedobject.1.dylib /tmp/upload
+cp /Users/dave/biosignalml/libbsml/osx/libbiosignalml.0.dylib     /tmp/upload/release
+cp /Users/dave/biosignalml/typedobject/osx/libtypedobject.1.dylib /tmp/upload/release
 #
-cp /Users/dave/biosignalml/libbsml/ubuntu/libbiosignalml.so.0     /tmp/upload
-cp /Users/dave/biosignalml/typedobject/ubuntu/libtypedobject.so.1 /tmp/upload
+cp /Users/dave/biosignalml/libbsml/ubuntu/libbiosignalml.so.0     /tmp/upload/release
+cp /Users/dave/biosignalml/typedobject/ubuntu/libtypedobject.so.1 /tmp/upload/release
 #
 pushd /tmp/upload > /dev/null
 #
 rm -f sha1.txt
+cd ./release
+echo "RELEASE" >> ../sha1.txt
 upload  biosignalml.dll windows/release/
 upload  biosignalml.lib windows/release/
 upload  typedobject.dll windows/release/
 upload  typedobject.lib windows/release/
-echo "" >> sha1.txt
-upload  biosignalml_d.dll windows/debug/
-upload  biosignalml_d.lib windows/debug/
-upload  typedobject_d.dll windows/debug/
-upload  typedobject_d.lib windows/debug/
+cd ../debug
+echo "DEBUG" >> ../sha1.txt
+upload  biosignalml.dll windows/debug/
+upload  biosignalml.lib windows/debug/
+upload  typedobject.dll windows/debug/
+upload  typedobject.lib windows/debug/
 #
-echo "" >> sha1.txt
+cd ../release
+echo "RELEASE" >> ../sha1.txt
 upload  libbiosignalml.0.dylib osx/
 upload  libtypedobject.1.dylib osx/
 #
-echo "" >> sha1.txt
+echo "" >> ../sha1.txt
 upload  libbiosignalml.so.0 linux/
 upload  libtypedobject.so.1 linux/
 #
-more sha1.txt
+more ../sha1.txt
 popd > /dev/null
